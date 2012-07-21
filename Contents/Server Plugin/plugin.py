5c22add1cc29af6ad5170b4810ccc40f62a57c98
Chad Francis <discgolfer1138@gmail.com>
2012-07-20 20:58:40 -0600

    got notifications working, but config is still static

--- Contents/Server Plugin/plugin.py
+++ Contents/Server Plugin/plugin.py
@@ -4,15 +4,10 @@
 # Copyright (c) 2012, Chad Francis. All rights reserved.
 # http://www.chadfrancis.com
 
-import os
-import sys
+import httplib, urllib, sys, os
 
-# Note the "indigo" module is automatically imported and made available inside
-# our global name space by the host process.
-
-################################################################################
 class Plugin(indigo.PluginBase):
-	########################################
+
 	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
 		indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
 		self.debug = True
@@ -20,15 +15,27 @@ class Plugin(indigo.PluginBase):
 	def __del__(self):
 		indigo.PluginBase.__del__(self)
 
-	########################################
+
 	def startup(self):
 		self.debugLog(u"startup called")
 
 	def shutdown(self):
 		self.debugLog(u"shutdown called")
 
-	########################################
-	# Plugin Actions object callbacks (pluginAction is an Indigo plugin action instance)
-	######################
+	# actions go here
 	def send(self, pluginAction):
-		self.debugLog("send action called:\n" + str(pluginAction))
+		conn = httplib.HTTPSConnection("api.pushover.net:443")
+		conn.request(
+			"POST",
+			"/1/messages",
+			urllib.urlencode({
+				# API token
+				"token": "Uxty2Vpz385ZLbCSNVZUWicQ2vPFEd",
+				# user token
+				"user": "PI7mxIKNpImeMlsn80elalL4iR02oI",
+				"title": pluginAction.props["txttitle"],
+				"message": pluginAction.props["txtmessage"],
+			}),
+			{ "Content-type": "application/x-www-form-urlencoded" }
+		)
+
